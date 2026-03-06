d83fe172496dd448024d865552b9bbff15f242af
rizkytanjung08 <aprilyaman24@gmail.com>
2026-03-06 20:34:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-// smooth scroll
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
 
-document.querySelectorAll("nav a").forEach(link => {
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-link.addEventListener("click", function(e){
+let particles = [];
 
-e.preventDefault();
+for(let i=0;i<100;i++){
+particles.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+size:Math.random()*2,
+speedX:(Math.random()-0.5)*0.5,
+speedY:(Math.random()-0.5)*0.5
+});
+}
 
-document.querySelector(this.getAttribute("href"))
-.scrollIntoView({
-behavior:"smooth"
+function animate(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+ctx.fillStyle="white";
+
+particles.forEach(p=>{
+
+p.x+=p.speedX;
+p.y+=p.speedY;
+
+if(p.x<0||p.x>canvas.width)p.speedX*=-1;
+if(p.y<0||p.y>canvas.height)p.speedY*=-1;
+
+ctx.beginPath();
+ctx.arc(p.x,p.y,p.size,0,Math.PI*2);
+ctx.fill();
+
 });
 
-});
+requestAnimationFrame(animate);
 
-});
+}
+
+animate();
